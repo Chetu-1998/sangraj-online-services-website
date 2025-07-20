d4fbcea05e78ce83947a31e32f701c0165a540b3
chetan <chetan.mali@jbdevs.xyz>
2025-07-20 19:27:02 +0530

    call to action text added

--- src/components/CallToAction.js
+++ src/components/CallToAction.js
@@ -9,8 +9,8 @@ const CallToAction = () => {
           <div className="col-xl-10">
             <div className="text-center">
               <h3>Call To Action</h3>
-              <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-              <a className="cta-btn" href="#">Call To Action</a>
+              <p>Empowering citizens with reliable CSC services — from government schemes to digital support, we’re here to assist every step of the way.</p>
+              <a className="cta-btn" href="#contact">Call To Action</a>
             </div>
           </div>
         </div>
